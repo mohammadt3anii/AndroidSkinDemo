d04663d1c9d478c4fc57cc9bc60eedc9688b6708
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-03 19:58:50 +0800

    Define Skinable interface, custom the View to implement this interface, and update skin function in the updateSkin method.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/Skinable.java
@@ -0,0 +1,11 @@
+package com.codearms.maoqiqi.skin.widget;
+
+/**
+ * 自定义视图需要实现该接口,并在updateSkin()方法中实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/3 19:50
+ */
+public interface Skinable {
+
+    void updateSkin();
+}
