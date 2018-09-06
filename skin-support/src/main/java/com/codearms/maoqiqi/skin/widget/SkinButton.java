959f632d7150184423ba7928cef134ee9a6a5b28
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-06 21:08:52 +0800

    Define SkinButton to inherit Button, implement Skinable interface, and update skin function.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinButton.java
@@ -0,0 +1,90 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import com.codearms.maoqiqi.skin.helper.SkinTextViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinButton继承Button,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/6 21:03
+ */
+@SuppressLint("AppCompatCustomView")
+public class SkinButton extends Button implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * TextView更新皮肤帮助类
+     */
+    private SkinTextViewHelper skinTextViewHelper;
+
+    public SkinButton(Context context) {
+        this(context, null);
+    }
+
+    public SkinButton(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.buttonStyle);
+    }
+
+    public SkinButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinTextViewHelper = new SkinTextViewHelper(this);
+        skinTextViewHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setTextAppearance(int resId) {
+        super.setTextAppearance(resId);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportTextAppearance(resId);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
+        super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.updateSkin();
+        }
+    }
+}
