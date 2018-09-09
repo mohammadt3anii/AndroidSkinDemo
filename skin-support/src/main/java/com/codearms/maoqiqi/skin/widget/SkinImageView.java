d319415db4468eb8a61fcc2c98901443fd13ba21
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-09 20:35:57 +0800

    Define SkinImageView to inherit ImageView, implement Skinable interface, and update skin function.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinImageView.java
@@ -0,0 +1,75 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.codearms.maoqiqi.skin.helper.SkinImageViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinImageView继承ImageView,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/9 20:27
+ */
+@SuppressLint("AppCompatCustomView")
+public class SkinImageView extends ImageView implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * ImageView更新皮肤帮助类
+     */
+    private SkinImageViewHelper skinImageViewHelper;
+
+    public SkinImageView(Context context) {
+        this(context, null);
+    }
+
+    public SkinImageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SkinImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinImageViewHelper = new SkinImageViewHelper(this);
+        skinImageViewHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        if (skinImageViewHelper != null) {
+            skinImageViewHelper.setSupportImageResource(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinImageViewHelper != null) {
+            skinImageViewHelper.updateSkin();
+        }
+    }
+}
