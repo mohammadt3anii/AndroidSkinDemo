c0b670dbcce8d34c0568ae2ae6b9709ff906371c
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-12 22:20:03 +0800

    Define SkinCalendarView to inherit CalendarView, implement Skinable interface, and implement update skin function using the defined SkinCalendarViewHelper.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/helper/SkinCalendarViewHelper.java
@@ -0,0 +1,26 @@
+package com.codearms.maoqiqi.skin.helper;
+
+import android.util.AttributeSet;
+import android.widget.CalendarView;
+
+/**
+ * CalendarView更新皮肤帮助类,CalendarView及子类都可以使用该帮助类
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/12 22:15
+ */
+public class SkinCalendarViewHelper extends SkinHelper<CalendarView> {
+
+    public SkinCalendarViewHelper(CalendarView view) {
+        super(view);
+    }
+
+    @Override
+    public void loadFromAttribute(AttributeSet attrs, int defStyleAttr) {
+
+    }
+
+    @Override
+    public void updateSkin() {
+
+    }
+}
---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinCalendarView.java
@@ -0,0 +1,63 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.CalendarView;
+
+import com.codearms.maoqiqi.skin.helper.SkinCalendarViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinCalendarView继承CalendarView,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/12 22:16
+ */
+public class SkinCalendarView extends CalendarView implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    private SkinCalendarViewHelper skinCalendarViewHelper;
+
+    public SkinCalendarView(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public SkinCalendarView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, android.R.attr.calendarViewStyle);
+    }
+
+    public SkinCalendarView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinCalendarView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinCalendarViewHelper = new SkinCalendarViewHelper(this);
+        skinCalendarViewHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinCalendarViewHelper != null) {
+            skinCalendarViewHelper.updateSkin();
+        }
+    }
+}
