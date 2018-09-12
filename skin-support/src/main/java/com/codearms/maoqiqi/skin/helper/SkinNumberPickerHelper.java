76186740ae3aca948e336cf064d5cca1595cd2f7
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-12 21:56:04 +0800

    Define SkinNumberPicker to inherit NumberPicker, implement Skinable interface, and implement update skin function using the defined SkinNumberPickerHelper.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/helper/SkinNumberPickerHelper.java
@@ -0,0 +1,26 @@
+package com.codearms.maoqiqi.skin.helper;
+
+import android.util.AttributeSet;
+import android.widget.NumberPicker;
+
+/**
+ * NumberPicker更新皮肤帮助类,NumberPicker及子类都可以使用该帮助类
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/12 21:45
+ */
+public class SkinNumberPickerHelper extends SkinHelper<NumberPicker> {
+
+    public SkinNumberPickerHelper(NumberPicker view) {
+        super(view);
+    }
+
+    @Override
+    public void loadFromAttribute(AttributeSet attrs, int defStyleAttr) {
+
+    }
+
+    @Override
+    public void updateSkin() {
+
+    }
+}
---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinNumberPicker.java
@@ -0,0 +1,64 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.NumberPicker;
+
+import com.codearms.maoqiqi.skin.helper.SkinNumberPickerHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinNumberPicker继承NumberPicker,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/12 21:48
+ */
+public class SkinNumberPicker extends NumberPicker implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * NumberPicker更新皮肤帮助类
+     */
+    private SkinNumberPickerHelper skinNumberPickerHelper;
+
+    public SkinNumberPicker(Context context) {
+        this(context, null);
+    }
+
+    public SkinNumberPicker(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.numberPickerStyle);
+    }
+
+    public SkinNumberPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinNumberPicker(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinNumberPickerHelper = new SkinNumberPickerHelper(this);
+        skinNumberPickerHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinNumberPickerHelper != null) {
+            skinNumberPickerHelper.updateSkin();
+        }
+    }
+}
