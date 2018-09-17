51478d60e52e2ac04aefeba4a4a207639b94ef9e
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-17 15:12:22 +0800

    Define SkinToolbar to inherit Toolbar, implement Skinable interface, and update skin function.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinToolbar.java
@@ -0,0 +1,104 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Toolbar;
+
+import com.codearms.maoqiqi.skin.helper.SkinToolbarHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinToolbar继承Toolbar,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/17 13:10
+ */
+public class SkinToolbar extends Toolbar implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * Toolbar更新皮肤帮助类
+     */
+    private SkinToolbarHelper skinToolbarHelper;
+
+    public SkinToolbar(Context context) {
+        this(context, null);
+    }
+
+    public SkinToolbar(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.toolbarStyle);
+    }
+
+    public SkinToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinToolbar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinToolbarHelper = new SkinToolbarHelper(this);
+        skinToolbarHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setTitleTextAppearance(Context context, int resId) {
+        super.setTitleTextAppearance(context, resId);
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.setSupportTitleTextAppearance(resId);
+        }
+    }
+
+    @Override
+    public void setSubtitleTextAppearance(Context context, int resId) {
+        super.setSubtitleTextAppearance(context, resId);
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.setSupportSubtitleTextAppearance(resId);
+        }
+    }
+
+    @Override
+    public void setLogo(int resId) {
+        super.setLogo(resId);
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.setSupportLogo(resId);
+        }
+    }
+
+    @Override
+    public void setNavigationIcon(int resId) {
+        super.setNavigationIcon(resId);
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.setSupportNavigationIcon(resId);
+        }
+    }
+
+    @Override
+    public void setPopupTheme(int resId) {
+        super.setPopupTheme(resId);
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.setSupportPopupTheme(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinToolbarHelper != null) {
+            skinToolbarHelper.updateSkin();
+        }
+    }
+}
