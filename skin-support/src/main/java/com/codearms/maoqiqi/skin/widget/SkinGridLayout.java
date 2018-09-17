65f414d4e19e6eb701b1b4d91fcac36f2dbf0d96
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-17 11:03:54 +0800

    Define SkinGridLayout to inherit GridLayout, implement Skinable interface, and update skin function.

---
+++ skin-support/src/main/java/com/codearms/maoqiqi/skin/widget/SkinGridLayout.java
@@ -0,0 +1,53 @@
+package com.codearms.maoqiqi.skin.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+
+/**
+ * 定义SkinGridLayout继承GridLayout,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/17 11:02
+ */
+public class SkinGridLayout extends GridLayout implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    public SkinGridLayout(Context context) {
+        this(context, null);
+    }
+
+    public SkinGridLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SkinGridLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SkinGridLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+    }
+}
