8fa289253fb71c33ba7bd185a517cfbf5704fbc2
maoqiqi <fengqi.mao.march@gmail.com>
2018-09-19 14:05:46 +0800

    Custom define SkinAppCompatAutoCompleteTextView, SkinAppCompatMultiAutoCompleteTextView, SkinAppCompatButton, SkinAppCompatRadioButton and SkinAppCompatCheckBox respectively inherit AppCompatAutoCompleteTextView, AppCompatMultiAutoCompleteTextView, AppCompatButton, AppCompatRadioButton and AppCompatCheckBox implement update skin.

---
+++ skin-support-appcompat/src/main/java/com/codearms/maoqiqi/skin/v7/widget/SkinAppCompatCheckBox.java
@@ -0,0 +1,104 @@
+package com.codearms.maoqiqi.skin.v7.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.util.AttributeSet;
+
+import com.codearms.maoqiqi.skin.helper.SkinCompoundButtonHelper;
+import com.codearms.maoqiqi.skin.helper.SkinTextViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+import com.codearms.maoqiqi.skin.widget.Skinable;
+
+/**
+ * 定义SkinSkinAppCompatCheckBox继承SkinAppCompatCheckBox,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/19 14:01
+ */
+public class SkinAppCompatCheckBox extends AppCompatCheckBox implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * TextView更新皮肤帮助类
+     */
+    private SkinTextViewHelper skinTextViewHelper;
+
+    /**
+     * CompoundButton更新皮肤帮助类
+     */
+    private SkinCompoundButtonHelper skinCompoundButtonHelper;
+
+    public SkinAppCompatCheckBox(Context context) {
+        this(context, null);
+    }
+
+    public SkinAppCompatCheckBox(Context context, AttributeSet attrs) {
+        this(context, attrs, android.support.v7.appcompat.R.attr.checkboxStyle);
+    }
+
+    public SkinAppCompatCheckBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinTextViewHelper = new SkinTextViewHelper(this);
+        skinTextViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinCompoundButtonHelper = new SkinCompoundButtonHelper(this);
+        skinCompoundButtonHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setTextAppearance(int resId) {
+        super.setTextAppearance(resId);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportTextAppearance(resId);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
+        super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        }
+    }
+
+    @Override
+    public void setButtonDrawable(int resId) {
+        super.setButtonDrawable(resId);
+        if (skinCompoundButtonHelper != null) {
+            skinCompoundButtonHelper.setSupportButtonDrawable(resId);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.updateSkin();
+        }
+        if (skinCompoundButtonHelper != null) {
+            skinCompoundButtonHelper.updateSkin();
+        }
+    }
+}
---
+++ skin-support-appcompat/src/main/java/com/codearms/maoqiqi/skin/v7/widget/SkinAppCompatMultiAutoCompleteTextView.java
@@ -0,0 +1,104 @@
+package com.codearms.maoqiqi.skin.v7.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatMultiAutoCompleteTextView;
+import android.util.AttributeSet;
+
+import com.codearms.maoqiqi.skin.helper.SkinAutoCompleteTextViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinTextViewHelper;
+import com.codearms.maoqiqi.skin.helper.SkinViewHelper;
+import com.codearms.maoqiqi.skin.widget.Skinable;
+
+/**
+ * 定义SkinAppCompatMultiAutoCompleteTextView继承AppCompatMultiAutoCompleteTextView,实现Skinable接口,实现更新皮肤功能
+ * Author: fengqi.mao.march@gmail.com
+ * Date: 2018/9/19 13:58
+ */
+public class SkinAppCompatMultiAutoCompleteTextView extends AppCompatMultiAutoCompleteTextView implements Skinable {
+
+    /**
+     * View更新皮肤帮助类
+     */
+    private SkinViewHelper skinViewHelper;
+
+    /**
+     * TextView更新皮肤帮助类
+     */
+    private SkinTextViewHelper skinTextViewHelper;
+
+    /**
+     * AutoCompleteTextView更新皮肤帮助类
+     */
+    private SkinAutoCompleteTextViewHelper skinAutoCompleteTextViewHelper;
+
+    public SkinAppCompatMultiAutoCompleteTextView(Context context) {
+        this(context, null);
+    }
+
+    public SkinAppCompatMultiAutoCompleteTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, android.support.v7.appcompat.R.attr.autoCompleteTextViewStyle);
+    }
+
+    public SkinAppCompatMultiAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        skinViewHelper = new SkinViewHelper(this);
+        skinViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinTextViewHelper = new SkinTextViewHelper(this);
+        skinTextViewHelper.loadFromAttribute(attrs, defStyleAttr);
+        skinAutoCompleteTextViewHelper = new SkinAutoCompleteTextViewHelper(this);
+        skinAutoCompleteTextViewHelper.loadFromAttribute(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        super.setBackgroundResource(resId);
+        if (skinViewHelper != null) {
+            skinViewHelper.setSupportBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setTextAppearance(int resId) {
+        super.setTextAppearance(resId);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportTextAppearance(resId);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
+        super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.setSupportCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        }
+    }
+
+    @Override
+    public void setDropDownBackgroundResource(int id) {
+        super.setDropDownBackgroundResource(id);
+        if (skinAutoCompleteTextViewHelper != null) {
+            skinAutoCompleteTextViewHelper.setSupportDropDownBackgroundResource(id);
+        }
+    }
+
+    @Override
+    public void updateSkin() {
+        if (skinViewHelper != null) {
+            skinViewHelper.updateSkin();
+        }
+        if (skinTextViewHelper != null) {
+            skinTextViewHelper.updateSkin();
+        }
+        if (skinAutoCompleteTextViewHelper != null) {
+            skinAutoCompleteTextViewHelper.updateSkin();
+        }
+    }
+}
